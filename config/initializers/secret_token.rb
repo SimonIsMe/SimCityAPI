d1207b9bed62669eb3eb446f22495e7809695c96
Szymon Skrzyński <skrzynski.szymon@gmail.com>
2012-12-13 11:01:09 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simcity::Application.config.secret_token = 'dce7bd96a9de24aa67ef0aae22cb90ed5674423cb2298a8b9c1320fe6ffb8e2dad18da99e8c6f67cc766586bd0ab1420e699658d3d99cbb93a81319ab148966d'
